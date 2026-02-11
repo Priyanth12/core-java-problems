cf10fa44729ff125433f307ff34039d62eb6ed9a
Priyanth R A <157478048+Priyanth12@users.noreply.github.com>
2026-02-11 12:01:00 +0530

    Add files via upload

---
+++ ascii_value_6.java
@@ -0,0 +1,9 @@
+package oops;
+import java.util.Scanner;
+public class ascii_value_6 {
+public static void main(String[]args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the String");
+	String str = sc.nextLine();	
+}
+}
