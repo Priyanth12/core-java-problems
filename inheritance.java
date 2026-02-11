c8c508d759e5fc07d08073d7e8f7bbd1dd71085d
Priyanth R A <157478048+Priyanth12@users.noreply.github.com>
2026-02-11 12:04:03 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,24 @@
+package oops;
+
+public class inheritance {
+	
+	public static void main(String[]args) {
+
+}
+}
+class ClassB {
+    static void student() {
+        System.out.println("This is from Class B");
+    }
+}
+class ClassA extends ClassB {
+    static void student1() {
+        System.out.println("tom");
+    }
+}
+	class classC extends ClassB {
+		static void student2() {
+			System.out.println("zoro");
+		}
+	
+}
